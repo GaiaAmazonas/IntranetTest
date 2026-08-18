189d6c597185fa35d3b38596f8e8cdae786a9652
GaiaIvanReyes <ireyes@gaiaamazonas.org>
2026-08-18 13:27:04 -0500

    Se agrega IntranetTest a la url para publicación
    Se agrega IntranetTest a la url para publicación de pruebas y exposición del demo

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: "export",
+  basePath: "/IntranetTest",
+  assetPrefix: "/IntranetTest/",
   images: {
     unoptimized: true,
   },
